c07568b8de00d15351d95aa815053e2840a34eca
hosu-kim <hosu@outlook.cz>
2026-03-07 22:55:26 +0100

    commit

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,54 +1,70 @@
 #include "ScalarConverter.hpp"
 
 void	ScalarConverter::convert(const std::string& literal) {
-	// handling special values
-	if (literal == "nan" || literal == "nanf" || literal == "+inf" ||
-		literal == "+inff" || literal == "-inf" || literal == "-inff") {
+	// this 'if' condition handles special values
+	if (literal == "nan" || literal == "nanf" ||
+		literal == "inf" || literal == "+inf" || literal == "-inf" ||
+		literal == "inff" || literal == "+inff" || literal == "-inff") {
 			std::cout << "char: impossible" << std::endl;
 			std::cout << "int: impossible" << std::endl;
+			// find(): 몇 번째 인덱스에 처음 나타나는지
+			// 못찾으면 npos를 반환한다
 			if (literal.find("ff") != std::string::npos || literal == "nanf") {
 				std::cout << "float: " << literal << std::endl;
+				// substr(): 문자열을 자른다 substr(시작 인덱스, 길이)
+				// 그러므로 +inff를 입력했을 때 double에서는 마지막 f가 잘린다.
 				std::cout << "double: " << literal.substr(0, literal.length() -1) << std::endl;
 			} else {
+				// +inf를 입력했을 때 float에서는 f가 추가된다.
 				std::cout << "float: " << literal << "f" << std::endl;
 				std::cout << "double: " << literal << std::endl;
 			}
 			return ;
 		}
-	
+
 	// conversion (literal -> double)
 	char* endptr = NULL;
-	double val = std::strtod(literal.c_str(), &endptr);
+	// strtod(): 문자열을 double 타입의 실수로 바꿔줌.
+	// strtod(문자열, 변환이 멈춘 지점의 주소)
+	double value = std::strtod(literal.c_str(), &endptr);
 
 	// Single character exception handling (e.g., 'a')
 	if (literal.length() == 1 && !std::isdigit(literal[0])) {
-		val = static_cast<double>(literal[0]);
-	} else if (*endptr != '\0' && std::string(endptr) != "f") {
+		value = static_cast<double>(literal[0]); // ('a' = 97.0)
+	} else if (*endptr != '\0' && std::string(endptr) != "f") { // e.g. 42abc, 42.42abc
 		std::cout << "Error: Invalid input" << std::endl;
 		return;
 	}
 
 	// printing each types on the console
-	// 1. char
+	// 1. char conversion
 	std::cout << "char: ";
-	if (std::isnan(val) || std::isinf(val) || val < 0 || val > 127)
+	// isnan(): is not a number? returns true or false
+	// isinf(): is infinite (overflow)? returns true or false
+	if (std::isnan(value) || std::isinf(value) || value < 0 || value > 127)
 		std::cout << "impossible" << std::endl;
-	else if (!std::isprint(static_cast<int>(val)))
+	// isprint(): is printable? returns true or false
+	else if (!std::isprint(static_cast<int>(value)))
 		std::cout << "Non displayable" << std::endl;
 	else
-		std::cout << "'" << static_cast<char>(val) << "'" << std::endl;
-
-	// 2. int
+		std::cout << "'" << static_cast<char>(value) << "'" << std::endl;
+// -----------------------------------------------------------------------------
+	// 2. int conversion
 	std::cout << "int: ";
-	if (val < std::numeric_limits<int>::min() ||
-		val > std::numeric_limits<int>:: max() ||
-		std::isnan(val))
+	// numeric_limits<type>::min()/max): returns the min/max value which the type can have
+	if (value < std::numeric_limits<int>::min() ||
+		value > std::numeric_limits<int>:: max() ||
+		// int can't have NaN
+		std::isnan(value))
 		std::cout << "impossible" << std::endl;
-	else
-		std::cout << static_cast<int>(val) << std::endl;
-	
-	// 3. float & double
-	std::cout << std::fixed << std::setprecision(1);
-	std::cout << "float: " << static_cast<float>(val) << "f" << std::endl;
-	std::cout << "double: " << val << std::endl;
+	else // success
+		std::cout << static_cast<int>(value) << std::endl;
+// -----------------------------------------------------------------------------
+	// 3. float & double conversion
+	// std::fixed + std::setpresicion:
+	//   e.g., if value is 42 -> after: 42.0
+	//         if value is 42.42 -> after: 42.4
+	std::cout << std::fixed << std::setprecision(1); // this code sets up input mode. doesn't print anything.
+	std::cout << "float: " << static_cast<float>(value) << "f" << std::endl; // +f => convention
+	std::cout << "double: " << value << std::endl;
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,3 +1,20 @@
+/*
+This project is about learning C++ type casts using
+1. `static_cast`: most used
+2. `dynamic_cast`: used to convert the type of classes
+3. `reinterpret_cast`: forced conversion
+4. `const_cast`: used to temporarily remove the const property
+
+And what the difference between using them and using C type casting (`(type)variable`)
+The difference is 
+1. C-style casting allows dangerous convert for e.g., from pointer to int
+but C++ type casts checks if the type casting is really possible
+2. easy to seach: when you search just `int` there're a lot of int keyword in the code,
+   so it's difficult to find where I cast variables.
+3. Clarify the intent: conveys the intent of type conversion (static_..., dynanic_...))
+
+*/
+
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
